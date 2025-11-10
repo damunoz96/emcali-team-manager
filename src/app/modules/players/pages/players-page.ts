0f3fbd040585e8588d44e68bcc1ccbc812456c9c
Daniel David Muñoz Morales <damunoz96@gmail.com>
2025-11-10 17:13:23 -0500

    feat: enhance games and players pages with improved layout and data handling

--- src/app/modules/players/pages/players-page.ts
+++ src/app/modules/players/pages/players-page.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
 
+const players = [
+  { name: 'Daniel', last_name: 'Munoz', position: 'F', games: 10, points: 200, fouls: 5 },
+  { name: 'Fabian', last_name: 'Duenas', position: 'PG', games: 10, points: 500, fouls: 2 },
+  { name: 'Camilo', last_name: 'Garcia', position: 'G', games: 10, points: 1000, fouls: 1 },
+  { name: 'Mateo', last_name: 'Munoz', position: 'F', games: 10, points: 200, fouls: 5 },
+  { name: 'Andrey', last_name: 'Duenas', position: 'PG', games: 10, points: 500, fouls: 2 },
+  { name: 'LeBron', last_name: 'Garcia', position: 'G', games: 10, points: 1000, fouls: 1 },
+];
+
 @Component({
   selector: 'players-page',
   templateUrl: './players-page.html',
 })
 export class PlayersPage {
+
+  dataSourcePlayers = players
 
 }
