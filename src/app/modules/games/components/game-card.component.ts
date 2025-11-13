0177931235acf8c96c1530d6d1dd625acce55e40
Daniel David Muñoz Morales <damunoz96@gmail.com>
2025-11-12 22:50:29 -0500

    feat: update GameCardComponent layout for improved responsiveness and styling

--- src/app/modules/games/components/game-card.component.ts
+++ src/app/modules/games/components/game-card.component.ts
@@ -13,30 +13,30 @@ export interface Game {
   selector: 'app-game-card',
   template: `
     <div
-      class="relative bg-card rounded-xl p-6 shadow-elevated border border-border hover:shadow-glow transition-all duration-300"
+      class="container relative bg-card rounded-xl p-6 shadow-elevated border border-border hover:shadow-glow transition-all duration-300"
     >
       <span class="absolute top-3 right-4 rounded-full text-xs text-accent font-semibold">
         {{ game.status }}
       </span>
       <div class="flex flex-col items-center gap-4">
-        <div class="flex items-center gap-6">
-          <div class="flex items-center gap-10">
-            <div class="text-3xl text-primary">Emcali</div>
-            <div class="text-3xl font-bold text-primary">{{ game.score?.emcali || 0 }}</div>
+        <div class="flex items-center gap-4 lg:gap-6 max-lg:pt-2">
+          <div class="flex items-center gap-4 lg:gap-10">
+            <div class="text-xl lg:text-3xl text-primary">Emcali</div>
+            <div class="text-xl lg:text-3xl font-bold text-primary">{{ game.score?.emcali || 0 }}</div>
           </div>
-          <div class="text-2xl text-muted-foreground">vs</div>
-          <div class="flex items-center gap-10">
-            <div class="text-3xl font-bold text-foreground">
+          <div class="text-md lg:text-2xl text-muted-foreground">vs</div>
+          <div class="flex items-center gap-2 lg:gap-10">
+            <div class="text-xl lg:text-3xl font-bold text-foreground">
               {{ game.score?.opponent || 0 }}
             </div>
-            <div class="text-3xl text-white">{{ game.opponent }}</div>
+            <div class="text-xl lg:text-3xl text-white">{{ game.opponent }}</div>
           </div>
         </div>
           <div class="flex items-center gap-5 w-full justify-center border-t border-border pt-2">
-            <span class=" text-muted-foreground font-light">{{
+            <span class=" text-muted-foreground font-ligh max-lg:text-sm">{{
               game.date
             }}</span>
-            <span class=" text-muted-foreground ">
+            <span class="text-muted-foreground max-lg:text-sm">
               {{ game.location }}
             </span>
           </div>
