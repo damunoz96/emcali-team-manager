90f92331c401b18a29d323b4cb2a163db4bd4145
Daniel David Muñoz Morales <damunoz96@gmail.com>
2025-11-13 19:22:39 -0500

    feat: integrate Supabase client and add database types for improved type safety

--- src/app/core/supabase/supabase.client.ts
+++ src/app/core/supabase/supabase.client.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { environment } from "../../../environments/environment";
+import { Database } from "../models/database.types";
 
 const supabaseUrl = environment.supabase_Url;
 const supabaseAnonKey = environment.supabase_Anon_Key;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
