1ad70046ea7a2a9ce3982ac4b68a7b0a0395d395
Daniel David Muñoz Morales <damunoz96@gmail.com>
2025-12-09 17:51:30 -0500

    [DELETE] remove angular material library

--- src/app/modules/home/pages/home-page.component.ts
+++ src/app/modules/home/pages/home-page.component.ts
@@ -1,11 +1,9 @@
 import { Component  } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
 
 
 @Component({
   selector: 'home-page',
   templateUrl: './home-page.component.html',
-  imports: [MatTableModule],
 })
 export class HomePage {
 
